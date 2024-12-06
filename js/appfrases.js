ce16e676a00047cffdbd500b4509447117944f9c
Jt0ny <jtonytipan@gmail.com>
2024-12-05 19:44:19 -0500

    Taller 15

--- js/appfrases.js
+++ js/appfrases.js
@@ -1,3 +1,4 @@
+
 const frases = [{frase:'El que no arriesga no gana', autor:'Robert Kiyosaki'},
     {frase:'Frase 2', autor:'Anthony Tipan'},
     {frase:'De tal palo tal astilla', autor:'Luis X'},
@@ -6,18 +7,59 @@ const frases = [{frase:'El que no arriesga no gana', autor:'Robert Kiyosaki'},
     {frase:'Antes que me muera quiero ser millonario', autor:'Yolanda'},
     
 ]
-
 const app = Vue.createApp({
-    methods: {
-       
-
-    },
-    data(){  
-        return{
-            listaFrases: frases
-        }
-    }
-
+ 
+   methods: {
+      agregarFrase() {
+         console.log(this.frase)
+         console.log(this.autor)
+         const nuevaFrase = {
+            frase: this.frase,
+            autor: this.autor
+         }
+         // El unshift se utiliza para agregar un elemento al inicio de un arreglo
+         this.listafrases.unshift(nuevaFrase)
+      },
+      agregarFraseFinal() {
+         console.log(this.frase)
+         console.log(this.autor)
+         const nuevaFrase = {
+            frase: this.frase,
+            autor: this.autor
+         }
+         // El push se utiliza para agregar un elemento al final de un arreglo
+         this.listafrases.push(nuevaFrase)
+      },
+      eventoKeyPress({charCode, cancelable, key, keyCode, target}) {
+ 
+         if(key === 'Enter'){
+            console.log('Evento');
+           // console.log(event);
+            console.log(charCode);
+            console.log(cancelable);
+            console.log(key);
+            console.log(keyCode);
+            console.log(target.baseURI);
+            this.agregarFraseFinal();
+         }
+      },
+ 
+      eventoKeyPressModificador() {
+         console.log('Presiono enter');
+         console.log('Evento keypress');
+         this.agregarFraseFinal();
+     }
+   },
+ 
+ 
+   data() {
+      return {
+         listafrases: frases,
+         frase: null,
+         //autor: 'sin autor'
+         autor: null
+      }
+   }
 })
-
+ 
 app.mount('#myApp')
